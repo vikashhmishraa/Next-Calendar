f9c21025c3ca388910316d47dd25751e00ddc29e
vikashhmishraa <vikashmishra749@gmail.com>
2023-01-19 05:35:49 +0530

    initial commit by vikas

---
+++ pages/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Calendar from './Calendar'
+
+
+export const index = () => {
+  return (
+    <>
+    <Calendar />
+    <div>
+      Hello 
+    </div>
+    
+    </>
+  )
+}
+
+
+export default index;
